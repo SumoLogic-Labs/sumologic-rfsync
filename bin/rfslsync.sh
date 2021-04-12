0149521a54a5ef8f2017663de882787fc82c8a4b
Wayne Schmidt <wschmidt@sumologic.com>
2021-04-12 18:09:06 +0900

    refactored wrapper script

--- bin/rfslsync.sh
+++ bin/rfslsync.sh
@@ -4,10 +4,17 @@ umask 022
 
 ulimit -Sv 1000000 
 
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cmdname="$BASEDIR/bin/rfslsync.py"
-cfgname="$BASEDIR/etc/rfslsync.cfg"
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASEDIR=$( dirname $MYDIR)
+
+BINDIR="$BASEDIR/bin"
+cmdname="$BINDIR/rfslsync.py"
+
+ETCDIR="$BASEDIR/etc"
+cfgname="$ETCDIR/etc/rfslsync.cfg"
+
 LOGDIR="$BASEDIR/log"
+logfile="$LOGDIR/output.log"
 
 complain_and_exit () {
    echo "ERROR: $2"
@@ -15,14 +22,10 @@ complain_and_exit () {
 }
 
 [ -d "$BASEDIR" ] || complain_and_exit "111" "Exiting. Cannot find: $BASEDIR"
-
-[ -f "$cmdname" ] || complain_and_exit "112" "Exiting. Cannot find: $cmdname"
-
-[ -f "$cfgname" ] || complain_and_exit "113" "Exiting. Cannot find: $cfgname"
-
 [ -d "$LOGDIR" ]  || mkdir -p $LOGDIR
 
-logfile="$BASEDIR/log/output.log"
+[ -f "$cmdname" ] || complain_and_exit "112" "Exiting. Cannot find: $cmdname"
+[ -f "$cfgname" ] || complain_and_exit "113" "Exiting. Cannot find: $cfgname"
 
 rm -f $logfile 
 touch $logfile
