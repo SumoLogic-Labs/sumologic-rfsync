4ef341b581dd33d2b8e29fe983e18ef598d4c161
Wayne Kirk Schmidt <wayne.kirk.schmidt@gmail.com>
2022-11-24 11:24:18 +0900

    added permissions for the content and indices as well

--- bin/terraform/rfsyncprep.folders.tf
+++ bin/terraform/rfsyncprep.folders.tf
@@ -10,6 +10,19 @@ output "deployment-folder-id" {
   value = "${sumologic_folder.recorded_future_base_folder.id}"
 }
 
+resource "sumologic_content_permission" "deployment-folder-permission" {
+    content_id = "${sumologic_folder.recorded_future_base_folder.id}"
+    notify_recipient = false
+    notification_message = "no_message"
+
+    permission {
+        permission_name = "View"
+        source_type = "org"
+        source_id = "${var.sumologic_org_id}"
+
+    }
+}
+
 data "local_file" "rfcontent" {
     filename = "${path.module}/json/recordedfuture_content.json"
 }
@@ -36,4 +49,17 @@ resource "sumologic_folder" "lookupfolder" {
 
 output "lookup-folder-id" {
   value = "${sumologic_folder.lookupfolder.id}"
+}
+
+resource "sumologic_content_permission" "lookup-folder-permission" {
+    content_id = "${sumologic_folder.lookupfolder.id}"
+    notify_recipient = false
+    notification_message = "no_message"
+
+    permission {
+        permission_name = "View"
+        source_type = "org"
+        source_id = "${var.sumologic_org_id}"
+
+    }
 }
